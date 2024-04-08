17fec40aab5ed5aa6a8130ccc50bed1693efafc4
44462576+PedroLantyer@users.noreply.github.com <pedrolantyer@hotmail.com>
2024-04-08 04:23:49 -0300

    Update C++ Folder

---
+++ C++/2861 - The Output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int questionCount;
+    scanf("%d", &questionCount);
+
+    std::string question;
+    for(int i = 0; i < questionCount; i++)
+    {
+        std::cin >> question;
+        printf("gzuz\n");
+    }
+    
+    return 0;
+}
